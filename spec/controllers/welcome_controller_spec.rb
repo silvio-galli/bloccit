ebe4c9b044c57beefbe500717df9a5c66aa9ff9b
silvio-galli <silvio.galli@gmail.com>
2015-11-16 12:17:36 +0100

    Added test to get faq template and then added route, action and faq template

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe WelcomeController, type: :controller do
 		end
 	end
 
+	describe "GET faq" do
+		it "gets the faq template" do
+			get :faq
+			expect(response).to render_template("faq")
+		end
+	end
+
 end
