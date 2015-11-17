bfcf79877fce51afe482c7be2fdfd1232a1455d0
silvio-galli <silvio.galli@gmail.com>
2015-11-17 22:50:56 +0100

    Modified db/seed.rb and added lib/random_data.rb to automatically insert posts and comments in Bloccit database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+include RandomData
+
+50.times do
+	Post.create!(
+		title: RandomData.random_sentence,
+		body: RandomData.random_paragraph
+		)	
+end
+posts = Post.all
+
+100.times do
+	Comment.create!(
+		post: posts.sample,
+		body: RandomData.random_paragraph
+		)
+end
+
+puts "Seeds finished!"
+puts "#{Post.count} posts created."
+puts "#{Comment.count} comments created."
