bcb09ca4b9aeea16d04e4c269cd9213ae79a01b5
silvio-galli <silvio.galli@gmail.com>
2015-11-18 10:34:35 +0100

    Add one unique post and one unique comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,27 @@ end
 puts "Seeds finished!"
 puts "#{Post.count} posts created."
 puts "#{Comment.count} comments created."
+
+# Adding one unique post.
+
+unique_post_title = RandomData.random_sentence
+unique_post_body = RandomData.random_paragraph
+
+=begin
+if (posts.any? {|post| post.title == unique_title}) || (posts.any? {|post| post.body == unique_body})
+	puts "The post is already in the database. Try again."
+else
+	Post.create!(
+		title: unique_title,
+		body: unique_body
+		)
+end
+=end
+Post.find_or_create_by(title: unique_post_title, body: unique_post_body)
+
+puts "Now we have #{Post.count} posts."
+
+unique_comment_body = RandomData.random_paragraph
+Comment.find_or_create_by(post: posts.sample, body: unique_comment_body)
+
+puts "Now we have #{Comment.count} comments."
