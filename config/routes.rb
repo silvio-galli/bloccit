b3034a037fa3702ee8e425fe6aa3fafbe39eb7ae
silvio-galli <silvio.galli@gmail.com>
2015-11-18 14:56:30 +0100

    Added Posts resource to Bloccit (posts controllers, posts views, posts spec)

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  
+  resources :posts
 
-  get 'welcome/about'
-
-  get 'welcome/contact'
-
-  get 'welcome/faq'
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
+
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -63,4 +62,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
