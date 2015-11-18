41ef014a851004b80d0a7c6366041a8e84d0066a
silvio-galli <silvio.galli@gmail.com>
2015-11-18 17:02:20 +0100

    Modified to SPAM the title of post every 5 posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
+
+  	primo = Post.first
+  	primo.title = "SPAM"
+  	primo.save!
+
+  	start = 6
+  	@posts.each do |post|
+  		if post.id == start
+  			post.title = "SPAM"
+  			start += 5
+  		end
+  	end
+
   end
 
   def show
@@ -11,4 +24,5 @@ class PostsController < ApplicationController
 
   def edit
   end
+
 end
