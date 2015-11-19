f22ec0a9550a9c8b56ff8c886b4f55107534552c
silvio-galli <silvio.galli@gmail.com>
2015-11-19 17:29:09 +0100

    Added edit and delete feature to Bloccit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,5 +26,33 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "The post was successfully updated."
+      redirect_to @post
+    else
+      flash[:error] = "Something went wrong. The post wasn't updated."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully"
+      redirect_to posts_path
+    else
+      flash[:error] = "Something went wrong. The \"#{@post.title}\" wasn't deleted."
+      render :show
+    end
+  end
+
 end
