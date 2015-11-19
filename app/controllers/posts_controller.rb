c74192802869cf4150d19b83cbd417ab505f1e69
silvio-galli <silvio.galli@gmail.com>
2015-11-19 10:09:13 +0100

    Updated app/models/post.rbwith the check_for_spam function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,6 @@
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
-
-  	primo = Post.first
-  	primo.title = "SPAM"
-  	primo.save!
-
-  	start = 6
-  	@posts.each do |post|
-  		if post.id == start
-  			post.title = "SPAM"
-  			start += 5
-  		end
-  	end
-
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
 	has_many :comments
+
+	after_save :check_for_spam
+
+	private
+	def check_for_spam
+		self.title = "SPAM" if id % 5 == 0
+		save
+	end
 end
