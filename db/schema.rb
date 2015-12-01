c4a4b10fb5e0fe131e47e033b6296abeed315671
silvio-galli <silvio.galli@gmail.com>
2015-12-01 14:40:05 +0100

    Added voting feature and rank order to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125175335) do
+ActiveRecord::Schema.define(version: 20151201131631) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20151125175335) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.string   "rating"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -67,6 +69,7 @@ ActiveRecord::Schema.define(version: 20151125175335) do
     t.boolean  "public",      default: true
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.string   "rating"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,5 +80,16 @@ ActiveRecord::Schema.define(version: 20151125175335) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
