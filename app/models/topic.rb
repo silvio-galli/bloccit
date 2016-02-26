ca43095be58e38c070be916d652edbc1106c2257
silvio-galli <silvio.galli@gmail.com>
2016-02-26 12:05:30 +0100

    Completed checkpoint 46, added private topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,15 @@ class Topic < ActiveRecord::Base
 
 	has_many :labelings, as: :labelable
 	has_many :labels, through: :labelings
+
+	scope :visible_to, -> (user) { user ? all : where(public: true) }
+=begin
+	def visible_to(user)
+		if user
+			Topics.all
+		else
+			Topics.where(public: true)
+		end
+	end
+=end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -28,4 +28,22 @@ RSpec.describe Topic, type: :model do
       end
     end
 
+    describe "scopes" do
+      before do
+        @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+        @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, pblic: false)
+      end
+
+      describe "visible_to(user)" do
+        it "returns all topics if the user is present" do
+          user = User.new
+          expect(Topic.visible_to(user)).to eq(Topic.all)
+        end
+
+        it "returns only public topics if user is nil" do
+          expect(Topoic.visible_to(nil)).to eq([@public_topic])
+        end
+      end
+    end
+
 end
